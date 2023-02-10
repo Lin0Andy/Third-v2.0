ccf164058bbc7a53de0939bff46099c817ab5676
Lin0Andy <121328479+Lin0Andy@users.noreply.github.com>
2023-02-10 23:29:40 +0600

    Update Inventory.java

--- src/Inventory.java
+++ src/Inventory.java
@@ -1,2 +1,29 @@
+import java.util.ArrayList;
+
 public class Inventory {
+    private ArrayList<Animal> products;
+
+    public Inventory(){
+        products = new ArrayList<Animal>();
+    }
+    public void addProduct(Animal product){
+        products.add(product);
+    }
+    public ArrayList<Animal> getProducts(){
+        return products;
+    }
+    public Animal getProduct(String pName){
+        for (Animal product : products){
+            if(product.name.equals(pName)){
+                return product;
+            }
+        }
+        return null;
+    }
+    public void listProducts(){
+        System.out.println("Product List: ");
+        for (Animal product : products){
+            System.out.println(product.toString());
+        }
+    }
 }
