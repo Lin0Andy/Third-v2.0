a52e6e9f139d500e2453a45393d454a8da53716d
Lin_Andy <221502@astanait.edu.kz>
2023-02-10 23:27:50 +0600

    All classes

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,8 @@
-public class Main {
+import java.lang.reflect.InvocationTargetException;
+import java.sql.SQLException;
 
+public class Main {
+    public static void main(String[] args){
+
+    }
 }
