af84523276974ddba47456fbae9f48ebc3c65a83
Lin0Andy <121328479+Lin0Andy@users.noreply.github.com>
2023-02-10 23:29:20 +0600

    Update Payment.java

--- src/Payment.java
+++ src/Payment.java
@@ -1,2 +1,34 @@
+import java.util.ArrayList;
+
 public class Payment {
+    private double amount;
+    private String paymentMethod;
+    private ArrayList<User> users;
+
+    public Payment(Animal animal, String paymentMethod){
+        this.amount = animal.cost;
+        this.paymentMethod = paymentMethod;
+    }
+    public double getAmount(){
+        return amount;
+    }
+    public void addUsers(User user){
+        users.add(user);
+    }
+    public ArrayList<User> getUsers() {
+        return users;
+    }
+
+    public String getPaymentMethod(){
+        return paymentMethod;
+    }
+    public String toString(User user) {
+        if(user.getWallet() >= getAmount()){
+            user.setWallet(user.getWallet() - getAmount());
+            return "Amount: $" + getAmount() + ", Payment Method: " + getPaymentMethod();
+        }
+        else{
+            return "Error! Not enough money for payment.";
+        }
+    }
 }
