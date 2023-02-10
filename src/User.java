9a59a1bf671c13c275acdd02fa07b54b5571ac7c
Nurzhan <113097957+ins0titud3@users.noreply.github.com>
2023-02-10 23:42:23 +0600

    Update User.java

--- src/User.java
+++ src/User.java
@@ -26,3 +26,134 @@ public class User extends Log_in{
             }
         }
     }
+    void buyAnimal(int type, int id) throws SQLException, ClassNotFoundException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
+        DataBase base = new DataBase();
+        switch (type){
+            case 1:
+                try{
+                    String sql1 = "SELECT * FROM birds WHERE id = " + id;
+                    System.out.println(sql1);
+                    ResultSet resultSet = base.statement.executeQuery(sql1);
+                    PreparedStatement preparedStatement = base.connection.prepareStatement("INSERT purchases(userName, typeOfAnimal, cost, age, weight, sex, abilities, diet, Life_cycle, color, species)" +
+                            "values (?, ?, ?, ?, ?, ?, ?, ?, ?, ? ,?)");
+                    while(resultSet.next()){
+                        preparedStatement.setString(1, this.username);
+                        preparedStatement.setString(2, "Bird");
+                        preparedStatement.setInt(3, resultSet.getInt(2));
+                        preparedStatement.setInt(4, resultSet.getInt(3));
+                        preparedStatement.setFloat(5, resultSet.getFloat(4));
+                        preparedStatement.setString(6, resultSet.getString(5));
+                        preparedStatement.setString(7, resultSet.getString(6));
+                        preparedStatement.setString(8, resultSet.getString(7));
+                        preparedStatement.setString(9, resultSet.getString(8));
+                        preparedStatement.setString(10, resultSet.getString(9));
+                        preparedStatement.setString(11,resultSet.getString(10));
+                        preparedStatement.executeUpdate();
+                    }
+
+                    String sql = "DELETE FROM birds where id = " + id;
+                    base.statement.executeUpdate(sql);
+                }
+                catch(Exception ex) {
+                    System.out.println("Введен неверный id");
+                    System.out.println(ex);
+                }
+                break;
+            case 2:
+                try {
+                    String sql1 = "SELECT * FROM fish WHERE id = " + id;
+                    ResultSet resultSet = base.statement.executeQuery(sql1);
+                    PreparedStatement preparedStatement = base.connection.prepareStatement("INSERT purchases(userName, typeOfAnimal, cost, age, weight, sex, abilities, diet, Life_cycle, color, species)" +
+                            "values (?, ?, ?, ?, ?, ?, ?, ?, ?, ? ,?)");
+                    preparedStatement.setString(1, this.username);
+                    preparedStatement.setString(2, "fish");
+                    preparedStatement.setInt(3, resultSet.getInt(2));
+                    preparedStatement.setInt(4, resultSet.getInt(3));
+                    preparedStatement.setFloat(5, resultSet.getFloat(4));
+                    preparedStatement.setString(6, resultSet.getString(5));
+                    preparedStatement.setString(7, resultSet.getString(6));
+                    preparedStatement.setString(8, resultSet.getString(7));
+                    preparedStatement.setString(9, resultSet.getString(8));
+                    preparedStatement.setString(10, resultSet.getString(9));
+                    preparedStatement.setString(11,resultSet.getString(10));
+                    preparedStatement.executeUpdate();
+                    String sql = "DELETE FROM fish where id = " + id;
+                    base.statement.executeUpdate(sql);
+                }
+                catch(Exception ex) {
+                    System.out.println("Введен неверный id");
+                    System.out.println(ex);
+                }
+                break;
+            case 3:
+                try{
+                    String sql1 = "SELECT * FROM dogs WHERE id = " + id;
+                    ResultSet resultSet = base.statement.executeQuery(sql1);
+                    PreparedStatement preparedStatement = base.connection.prepareStatement("INSERT purchases(userName, typeOfAnimal, cost, age, weight, sex, color, abilities,  species)" +
+                            "values (?, ?, ?, ?, ?, ?, ?, ?, ?)");
+                    preparedStatement.setString(1, this.username);
+                    preparedStatement.setString(2, "Dog");
+                    preparedStatement.setInt(3, resultSet.getInt(2));
+                    preparedStatement.setInt(4, resultSet.getInt(3));
+                    preparedStatement.setFloat(5, resultSet.getFloat(4));
+                    preparedStatement.setString(6, resultSet.getString(5));
+                    preparedStatement.setString(7, resultSet.getString(6));
+                    preparedStatement.setString(8, resultSet.getString(7));
+                    preparedStatement.setString(9, resultSet.getString(8));
+                    preparedStatement.executeUpdate();
+                    String sql = "DELETE FROM dogs where id = " + id;
+                    base.statement.executeUpdate(sql);
+                }
+                catch(Exception ex){
+                    System.out.println("Введен неверный id");
+                    System.out.println(ex);
+                }
+                break;
+            case 4:
+                try{
+                    String sql1 = "SELECT * FROM cats WHERE id = " + id;
+                    ResultSet resultSet = base.statement.executeQuery(sql1);
+                    PreparedStatement preparedStatement = base.connection.prepareStatement("INSERT purchases(userName, typeOfAnimal, cost, age, weight, sex,  species)" +
+                            "values (?, ?, ?, ?, ?, ?, ?, ?)");
+                    preparedStatement.setString(1, this.username);
+                    preparedStatement.setString(2, "Bird");
+                    preparedStatement.setInt(3, resultSet.getInt(2));
+                    preparedStatement.setInt(4, resultSet.getInt(3));
+                    preparedStatement.setFloat(5, resultSet.getFloat(4));
+                    preparedStatement.setString(6, resultSet.getString(5));
+                    preparedStatement.setString(7, resultSet.getString(6));
+                    preparedStatement.setString(8, resultSet.getString(7));
+                    preparedStatement.executeUpdate();
+                    String sql = "DELETE FROM cats where id = " +  id;
+                    base.statement.executeUpdate(sql);
+                }
+                catch(Exception ex){
+                    System.out.println("Введен неверный id");
+                    System.out.println(ex);
+                }
+                break;
+            case 5:
+                try {
+                    String sql1 = "SELECT * FROM rodents WHERE id = " + id;
+                    ResultSet resultSet = base.statement.executeQuery(sql1);
+                    PreparedStatement preparedStatement = base.connection.prepareStatement("INSERT purchases(userName, typeOfAnimal, cost, age, weight, sex, color, species)" +
+                            "values (?, ?, ?, ?, ?, ?, ?, ?)");
+                    preparedStatement.setString(1, this.username);
+                    preparedStatement.setString(2, "Bird");
+                    preparedStatement.setInt(3, resultSet.getInt(2));
+                    preparedStatement.setInt(4, resultSet.getInt(3));
+                    preparedStatement.setFloat(5, resultSet.getFloat(4));
+                    preparedStatement.setString(6, resultSet.getString(5));
+                    preparedStatement.setString(7, resultSet.getString(6));
+                    preparedStatement.setString(8, resultSet.getString(7));
+                    preparedStatement.executeUpdate();
+                    String sql = "DELETE FROM rodents where id = " + id;
+                    base.statement.executeUpdate(sql);
+                }
+                catch(Exception ex){
+                    System.out.println("Введен неверный id");
+                    System.out.println(ex);
+                }
+                break;
+        }
+    }
